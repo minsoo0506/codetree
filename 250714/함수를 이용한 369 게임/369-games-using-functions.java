9d1c5d47a0c05d37e86b63c0f8e8b25523883ebb
박민수 <68321360+minsoo0506@users.noreply.github.com>
2025-07-14 18:33:47 +0900

    Title: 함수를 이용한 369 게임, Time: 143ms, Memory: 10MB, Status: Wrong Answer - Codetree

---
+++ 250714/함수를 이용한 369 게임/369-games-using-functions.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+public class Main {
+    public static boolean is3Multiple(int n){
+        return n % 3 == 0;
+    }
+
+    public static boolean isMagicNum(int n){
+        int digit1 = n / 10;
+        int digit2 = n % 10;
+
+        if(digit1 == 3 || digit1 == 6 || digit1 == 9){
+            return true;
+        } else if(digit2 == 3 || digit2 == 6 || digit2 == 9){
+            return true;
+        } else if (is3Multiple(n)){
+            return true;
+        }
+
+        return false;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int A = sc.nextInt();
+        int B = sc.nextInt();
+        // Please write your code here.
+
+        int count = 0;
+        for(int i = A; i <= B; i++){
+            if(isMagicNum(i)) count++;
+        }
+
+        System.out.println(count);
+    }
+}
