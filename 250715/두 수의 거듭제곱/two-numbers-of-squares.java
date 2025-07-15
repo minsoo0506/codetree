aafd223090148c6fe53aed9e8c904f4307496807
박민수 <68321360+minsoo0506@users.noreply.github.com>
2025-07-15 19:04:33 +0900

    Title: 두 수의 거듭제곱, Time: 137ms, Memory: 10MB, Status: Passed - Codetree

--- 250715/두 수의 거듭제곱/two-numbers-of-squares.java
+++ 250715/두 수의 거듭제곱/two-numbers-of-squares.java
@@ -1,16 +1,21 @@
 import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int a = sc.nextInt();
-        int b = sc.nextInt();
-        // Please write your code here.
+    public static int square(int a, int b){
         int result = 1;
         for(int i = 0; i < b; i++){
             result *= a;
         }
 
-        System.out.println(result);
+        return result;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        // Please write your code here.
+
+        System.out.println(square(a, b));
     }
 }
