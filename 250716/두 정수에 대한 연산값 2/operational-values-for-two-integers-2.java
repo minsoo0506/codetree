58339973c8ce9eeddce1a7821462e17d1b288616
박민수 <68321360+minsoo0506@users.noreply.github.com>
2025-07-16 16:44:25 +0900

    Title: 두 정수에 대한 연산값 2, Time: 133ms, Memory: 10MB, Status: Passed - Codetree

---
+++ 250716/두 정수에 대한 연산값 2/operational-values-for-two-integers-2.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+
+class IntWrapper{
+    int value;
+
+    public IntWrapper(int value){
+        this.value = value;
+    }
+}
+
+public class Main {
+    public static void modify(IntWrapper A, IntWrapper B){
+        if(A.value > B.value){
+            A.value *= 2;
+            B.value += 10;
+        } else {
+            B.value *= 2;
+            A.value += 10;
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        // Please write your code here.
+
+        IntWrapper A = new IntWrapper(a);
+        IntWrapper B = new IntWrapper(b);
+
+        modify(A, B);
+
+        System.out.println(A.value + " " + B.value);
+    }
+}
