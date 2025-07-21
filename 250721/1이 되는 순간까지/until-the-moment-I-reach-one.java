3feb82985d598fc0da3bd383da3b572bf3431afc
박민수 <68321360+minsoo0506@users.noreply.github.com>
2025-07-21 14:38:00 +0900

    Title: 1이 되는 순간까지, Time: 124ms, Memory: 10MB, Status: Passed - Codetree

---
+++ 250721/1이 되는 순간까지/until-the-moment-I-reach-one.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+public class Main {
+    public static int F(int n){
+        if(n == 1) return 0;
+
+        if (n % 2 == 0) n /= 2;
+        else n /= 3;
+
+        return 1 + F(n); 
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        // Please write your code here.
+
+        System.out.println(F(n));
+    }
+}
