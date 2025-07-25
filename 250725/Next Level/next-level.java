ca295d8c465c958443490149bf2d27cf2d091ac6
박민수 <68321360+minsoo0506@users.noreply.github.com>
2025-07-25 15:01:15 +0900

    Title: Next Level, Time: 131ms, Memory: 10MB, Status: Wrong Answer - Codetree

---
+++ 250725/Next Level/next-level.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+class User {
+    String id;
+    int level;
+
+    public User(String id, int level){
+        this.id = id;
+        this.level = level;
+    }
+
+    public void print(){
+        System.out.println("user " + id + " lv " + level);
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String id = sc.next();
+        int level = sc.nextInt();
+        // Please write your code here.
+
+        User codetree = new User("codetree", 10);
+        User hello = new User("hello", 28);
+
+        codetree.print();
+        hello.print();
+    }
+}
