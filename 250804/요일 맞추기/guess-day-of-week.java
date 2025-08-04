4f69dbdb5c3b1558c8518419f60a27c663244552
박민수 <68321360+minsoo0506@users.noreply.github.com>
2025-08-04 17:41:28 +0900

    Title: 요일 맞추기, Time: 122ms, Memory: 10MB, Status: Wrong Answer - Codetree

---
+++ 250804/요일 맞추기/guess-day-of-week.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+
+public class Main {
+    public static String calculateDay(int m1, int d1, int m2, int d2) {
+        int[] endDate = {0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31}; 
+        String[] day = {"Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"};
+
+        int end = 0;
+        for(int i = 1; i <= m2; i++){
+            end += endDate[i];
+        }
+        end += d2;
+
+        int start = 0;
+        for(int i = 1; i <= m1; i++){
+            start += endDate[i];
+        }
+        start += d1;
+
+        return day[(end - start + 7) % 7];
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int m1 = sc.nextInt();
+        int d1 = sc.nextInt();
+        int m2 = sc.nextInt();
+        int d2 = sc.nextInt();
+        
+        System.out.println(calculateDay(m1, d1, m2, d2));
+    }
+}
