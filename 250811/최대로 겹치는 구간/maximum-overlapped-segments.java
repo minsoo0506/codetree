a34725c5d70149da184beb6fafa86dbdb215ebb5
박민수 <68321360+minsoo0506@users.noreply.github.com>
2025-08-11 16:34:34 +0900

    Title: 최대로 겹치는 구간, Time: 148ms, Memory: 10MB, Status: Passed - Codetree

--- 250811/최대로 겹치는 구간/maximum-overlapped-segments.java
+++ 250811/최대로 겹치는 구간/maximum-overlapped-segments.java
@@ -4,15 +4,18 @@ import java.util.HashMap;
 import java.util.Collections;
 
 public class Main {
+
     public static void main(String[] args) {
+
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
         Map<Integer, Integer> map = new HashMap<>();
+
         for (int i = 0; i < n; i++) {
             int x1 = sc.nextInt();
             int x2 = sc.nextInt();
 
-            for(int j = x1; j <= x2; j++){
+            for(int j = x1; j < x2; j++){
                 map.put(j, map.getOrDefault(j, 0) + 1);
             }
         }
